d785686df34a2a73624155eec446194ab4cc9cdc
AWilson <dreamy26@icloud.com>
2020-10-13 22:51:48 -0400

    add notes file, instructions file, and scaffold project

---
+++ decisionMaker/decisionMaker/Program.cs
@@ -0,0 +1,29 @@
+﻿// Requirements are listed in notes.md 
+
+using System;
+
+namespace decisionMaker
+{
+    class MainClass
+    {
+        private static string response;
+
+        public static void Main(string[] args)
+        {
+            //req8
+            Console.WriteLine("What is your name? (type your name and press enter)");
+
+            
+            do
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Continue? y/n");
+                response = Console.ReadLine();
+            } while (response == "y");
+            // req7
+            Console.WriteLine("Bye");
+
+            
+        }
+    }
+}

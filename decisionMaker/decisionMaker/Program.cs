11c309b2b6556aa65881261827f956bece288b8c
AWilson <dreamy26@icloud.com>
2020-10-14 15:45:41 -0400

    add additional requirements for assignment

--- decisionMaker/decisionMaker/Program.cs
+++ decisionMaker/decisionMaker/Program.cs
@@ -12,26 +12,56 @@ namespace decisionMaker
         {
             string userName = string.Empty;
             int userNumber = 0;
+            int i;
 
             //req8
             Console.WriteLine("What is your name? (type your name and press enter)");
             userName = Console.ReadLine();
 
-            
+
 
             do
             {
                 Console.WriteLine("Enter a number between 1 & 100");
                 userNumber = Int32.Parse(Console.ReadLine());
 
-                Console.WriteLine("Continue? y/n");
-                response = Console.ReadLine();
-            } while (response.ToLower() == "y");
+                if (userNumber % 2 == 0)
+                {
+                    if (userNumber < 25)
+                    {
+                        Console.Write("Even and less than 25");
+                        Console.Read();
+                    }
+                    else if (userNumber > 60)
+                    {
+                        Console.Write($"{userNumber} Even");
+                        Console.Read();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Even");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    // since the outcome is the same for odd numbers greater than and less than 60 there is no test condition 
+                    Console.Write($"{userNumber} Odd");
+                    Console.Read();
 
-            // req7
-            Console.WriteLine("Bye");
 
-            
+                }
+                    Console.WriteLine("Continue? y/n");
+                    response = Console.ReadLine();
+                   
+                } while (response.ToLower() == "y") ;
+
+                // req7
+                Console.WriteLine($"Byyyeee {userName}");
+            }
         }
     }
-}
+
+
+    
+

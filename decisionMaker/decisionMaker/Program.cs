bf7e037b67e051aa7186bdbe2e3da3949f0dc391
AWilson <dreamy26@icloud.com>
2020-10-14 12:40:42 -0400

    added variables that will be stored from user

--- decisionMaker/decisionMaker/Program.cs
+++ decisionMaker/decisionMaker/Program.cs
@@ -10,16 +10,24 @@ namespace decisionMaker
 
         public static void Main(string[] args)
         {
+            string userName = string.Empty;
+            int userNumber = 0;
+
             //req8
             Console.WriteLine("What is your name? (type your name and press enter)");
+            userName = Console.ReadLine();
 
             
+
             do
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine("Enter a number between 1 & 100");
+                userNumber = Int32.Parse(Console.ReadLine());
+
                 Console.WriteLine("Continue? y/n");
                 response = Console.ReadLine();
-            } while (response == "y");
+            } while (response.ToLower() == "y");
+
             // req7
             Console.WriteLine("Bye");
 
